2d94d7a11a2fcf9a9913ed9e2ed8aef23942d3d4
Vinicius Fraga <vinifragam@gmail.com>
2019-11-27 08:23:53 -0300

    Edplatform response hardcoded

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import FilterController from './app/controllers/FilterController';
 import EnterpriseController from './app/controllers/EnterpriseController';
+import EdPlatformController from './app/controllers/EdPlatformController';
 
 const routes = new Router();
 
 // Categories
-routes.get('/', FilterController.index);
+routes.get('/filters', FilterController.index);
 
-routes.post('/enterprise', EnterpriseController.index);
+// Enterprises
+routes.post('/enterprises', EnterpriseController.index);
 
+// Ed Platforms
+routes.post('/edplatforms', EdPlatformController.index);
 export default routes;
